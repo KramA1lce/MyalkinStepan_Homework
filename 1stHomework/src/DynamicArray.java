7f176b4725cde889b41142ff2e086953e769392d
KramA1lce <myalkin.stepan@mail.ru>
2021-02-26 10:16:50 +0300

    added get function
    get and initcap added, array throws exception if number of element is lower than array capacity or below zero

--- 1stHomework/src/DynamicArray.java
+++ 1stHomework/src/DynamicArray.java
@@ -6,6 +6,10 @@ public class DynamicArray<A> {
         arrghay = ((A[]) new Object[initsize]);
     }
 
+    public int initcap() {
+        return arrghay.length;
+    }
+
     public DynamicArray(int newsize) {
         if (newsize > -1) {
             arrghay = ((A[]) new Object[initsize]);
@@ -15,5 +19,11 @@ public class DynamicArray<A> {
         }
     }
 
+    public A get(int number) {
+        if (initcap() <= number || number < 0) {
+            throw new RuntimeException();
+        }
+        return arrghay[number];
+    }
     
 }
