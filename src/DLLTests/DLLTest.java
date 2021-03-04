1ab1be530d7ae0260934a7edf37cadf25b0c7c75
Stepan Myalkin <myalkin.stepan@mail.ru>
2021-03-04 13:56:23 +0300

    Added 2 tests
    added tests for adding functions

---
+++ src/DLLTests/DLLTest.java
@@ -0,0 +1,13 @@
+package DLLTests;
+import DoubleLinkedLists.DoubleLinkedLists;
+import org.junit.jupiter.api.Test;
+
+public class DLLTest {
+    @Test
+    public void DLLTests() {
+        DoubleLinkedLists DoubleLinkedLists = new DoubleLinkedLists();
+        DoubleLinkedLists.addHead(666);
+        DoubleLinkedLists.addTail(333);
+
+    }
+}
