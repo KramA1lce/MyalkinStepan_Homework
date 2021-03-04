67dded7eb0e16403ae78d244e7e9af73feb0011c
Stepan Myalkin <myalkin.stepan@mail.ru>
2021-03-04 13:51:18 +0300

    Added 3 functions
    Forgot to commit after completing each of the functions. Added emptiness check, adding of the first element, adding of the tail

--- src/DoubleLinkedLists/DoubleLinkedLists.java
+++ src/DoubleLinkedLists/DoubleLinkedLists.java
@@ -7,4 +7,30 @@ public class DoubleLinkedLists<L> {
     public DoubleLinkedLists() {
         head = tail = null;
     }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addHead(L element) {
+        DoubleLinkedListElement<L> newElement = new DoubleLinkedListElement<L>(element);
+        newElement.next = head;
+        newElement.previous = null;
+        if (isEmpty()) head = tail = newElement;
+        else  {
+            head.previous = newElement;
+            head = newElement;
+        }
+    }
+
+    public void addTail(L element) {
+        DoubleLinkedListElement<L> newElement = new DoubleLinkedListElement<L>(element);
+        newElement.next = null;
+        newElement.previous = tail;
+        if (isEmpty())  head = tail = newElement;
+        else {
+            tail.next = newElement;
+            tail = newElement;
+        }
+    }
 }
