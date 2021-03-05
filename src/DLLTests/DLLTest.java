26e1706d5a8e527c7fabdda3c9e7eeb534d04380
Stepan Myalkin <myalkin.stepan@mail.ru>
2021-03-05 10:14:10 +0300

    Tests update
    Note: tests still a bloody mess

--- src/DLLTests/DLLTest.java
+++ src/DLLTests/DLLTest.java
@@ -1,13 +1,21 @@
 package DLLTests;
 import DoubleLinkedLists.DoubleLinkedLists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-public class DLLTest {
+public class DLLTest extends Assertions {
     @Test
-    public void DLLTests() {
+    public void addHeadTest() {
         DoubleLinkedLists DoubleLinkedLists = new DoubleLinkedLists();
         DoubleLinkedLists.addHead(666);
+        assertEquals(666, );
+    }
+    @Test
+
         DoubleLinkedLists.addTail(333);
 
+    @Test
+    public void getSize() {
+        DoubleLinkedLists.getSize();
     }
 }
